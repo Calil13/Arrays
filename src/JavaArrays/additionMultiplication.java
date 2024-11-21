4692659457927008cedae3af1a7549c65fafca93
Calil13 <ahmadovcalil13@gmail.com>
2024-11-21 23:54:22 +0400

    First Commit

--- src/JavaArrays/additionMultiplication.java
+++ src/JavaArrays/additionMultiplication.java
@@ -4,20 +4,20 @@ public class additionMultiplication {
 		
 	public static void main(String[] args) {
 		
-		int dizi[] = {3,12,5,8};
+		int array[] = {5,10,11,9};
 		int toplam1 = 0;
 		int carpim = 1;
 		int toplam2 = 0;
 		int toplam3 = 0;
 		
-		for (int i = 0; i < dizi.length; i++) {
-			toplam1 += dizi[i];
-			carpim *= dizi[i];
+		for (int i = 0; i < array.length; i++) {
+			toplam1 += array[i];
+			carpim *= array[i];
 			
-			if(dizi[i] % 2 != 0) {
-				toplam2 += dizi[i];
-			}else if(dizi[i] % 2 == 0) {
-				toplam3 += dizi[i];
+			if(array[i] % 2 != 0) {
+				toplam2 += array[i];
+			}else if(array[i] % 2 == 0) {
+				toplam3 += array[i];
 			}
 		}
 		System.out.println("1. Toplam : " + toplam1);
